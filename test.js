5fc3d86c14a0acc14683a1ca593a91ae73313422
花裤衩 <panfree23@gmail.com>
2019-10-10 21:37:09 +0800

    update

--- test.js
+++ test.js
@@ -1,63 +1,23 @@
-const MarkdownIt = require('markdown-it')
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
 const mkdirp = require('mkdirp')
 const glob = require('glob')
+const commonMark = require('commonmark')
 
-const md = new MarkdownIt()
-dir = './images'
+const outputDir = './images'
+const inputDir = './docs/zh'
 
-//创建目录
-mkdirp(dir, function(err) {
+mkdirp(outputDir, function(err) {
   if (err) {
     console.log(err)
   }
 })
 
-//解析需要遍历的文件夹，我这以E盘根目录为例
-var filePath = path.resolve('./docs/zh')
-
-// //调用文件遍历方法
-// fileDisplay(filePath);
-
-/**
- * 文件遍历方法
- * @param filePath 需要遍历的文件路径
- */
-function fileDisplay(filePath) {
-  //根据文件路径读取文件，返回文件列表
-  fs.readdir(filePath, function(err, files) {
-    if (err) {
-      console.warn(err)
-    } else {
-      //遍历读取到的文件列表
-      files.forEach(function(filename) {
-        //获取当前文件的绝对路径
-        var filedir = path.join(filePath, filename)
-        //根据文件路径获取文件信息，返回一个fs.Stats对象
-        fs.stat(filedir, function(eror, stats) {
-          if (eror) {
-            console.warn('获取文件stats失败')
-          } else {
-            var isFile = stats.isFile() //是文件
-            var isDir = stats.isDirectory() //是文件夹
-            if (isFile && filedir.endsWith('.md')) {
-              console.log(filedir)
-            }
-            if (isDir) {
-              fileDisplay(filedir) //递归，如果是文件夹，就继续遍历该文件夹下面的文件
-            }
-          }
-        })
-      })
-    }
-  })
-}
-
-let index = 0
-async function downloadImage(url, imgName) {
-  const writer = fs.createWriteStream(`./images/${index++}.png`)
+async function downloadImage(url) {
+  const s = url.split('/')
+  const name = s[s.length - 1]
+  const writer = fs.createWriteStream(`${outputDir}/${name}`)
 
   const response = await axios({
     url,
@@ -73,57 +33,69 @@ async function downloadImage(url, imgName) {
   })
 }
 
-md.renderer.rules.image = function(tokens, idx) {
-  var token = tokens[idx]
-  var srcIndex = token.attrIndex('src')
-  var url = token.attrs[srcIndex][1]
-  downloadImage(url)
-  return ''
+const getAllImg = markdown => {
+  if (!markdown) return
+  const parsed = new commonMark.Parser().parse(markdown)
+  const walker = parsed.walker()
+  let event
+  const nodeList = []
+  while ((event = walker.next())) {
+    const node = event.node
+    if (node.type === 'image' && node.destination) {
+      nodeList.push(node)
+    }
+  }
+  const list = nodeList.map(node => node.destination)
+  const uniqueSrcList = [...new Set(list)]
+
+  return {
+    list,
+    uniqueSrcList,
+    nodeList
+  }
 }
 
-exports.pathResolve = dir => {
-  return path.join(__dirname, '..', dir)
-}
-
-readFile = file => {
+const readFile = file => {
   return new Promise((resolve, reject) => {
-    const dirpath = file
-    console.log(dirpath)
-    fs.readFile(dirpath, 'utf8', (err, data) => {
+    const dirPath = file
+
+    fs.readFile(dirPath, 'utf8', (err, data) => {
       if (err) {
-        reject()
+        reject(err)
       }
-      resolve(data)
+
+      const images = getAllImg(data)
+      if (images) {
+        resolve(images.uniqueSrcList)
+      }
     })
   })
 }
 
 async function generate() {
-  // const files = ['./docs/guide/README.md', './docs/guide/advanced/cdn.md']
-  // var content = []
-  // for (const file of files) {
-  //   content.push(await readFile(file))
-  // }
+  try {
+    glob(
+      '**/*.md',
+      {
+        cwd: path.resolve(process.cwd(), inputDir)
+      },
+      async function(er, files) {
+        let imgs = []
+        for (const file of files) {
+          const data = await readFile(`${inputDir}/${file}`)
+          imgs = imgs.concat(data)
+        }
 
-  // var result = md.render(content.join(','));
+        imgs = [...new Set(imgs)]
 
-  glob(
-    '**/*.md',
-    {
-      cwd: path.resolve(process.cwd(), './docs/zh')
-    },
-    function(er, files) {
-      console.log(files)
-      // files is an array of filenames.
-      // If the `nonull` option is set, and nothing
-      // was found, then files is ["**/*.js"]
-      // er is an error object or null.
-    }
-  )
+        for (const url of imgs) {
+          downloadImage(url)
+        }
+      }
+    )
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-console.log(process.cwd())
-
 generate()
-
-// console.log(result)
